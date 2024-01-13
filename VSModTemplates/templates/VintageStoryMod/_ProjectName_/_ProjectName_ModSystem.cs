24f5aca918cdf831372a745baaaecf97f82dfebb
Th3Dilli <th3dilli@gmx.at>
2024-01-14 00:53:04 +0100

    Fix: modid is now converted to lowercase Fix: assets folder not exluded if --AddSampleCode was missing Added: --AddAssetFolder Fix: some code cleanup

--- VSModTemplates/templates/VintageStoryMod/_ProjectName_/_ProjectName_ModSystem.cs
+++ VSModTemplates/templates/VintageStoryMod/_ProjectName_/_ProjectName_ModSystem.cs
@@ -5,27 +5,26 @@ using Vintagestory.API.Config;
 #endif
 using Vintagestory.API.Common;
 
-namespace _ProjectName_
+namespace _ProjectName_;
+
+public class _ProjectName_ModSystem : ModSystem
 {
-    public class _ProjectName_ModSystem : ModSystem
-    {
 #if( AddSampleCode )
-        // Called on server and client
-        // Useful for registering block/entity classes on both sides
-        public override void Start(ICoreAPI api)
-        {
-            api.Logger.Notification("Hello from template mod: " + api.Side);
-        }
-
-        public override void StartServerSide(ICoreServerAPI api)
-        {
-            api.Logger.Notification("Hello from template mod server side: " + Lang.Get("_ProjectName_:hello"));
-        }
-
-        public override void StartClientSide(ICoreClientAPI api)
-        {
-            api.Logger.Notification("Hello from template mod client side: " + Lang.Get("_ProjectName_:hello"));
-        }
-#endif
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+    public override void Start(ICoreAPI api)
+    {
+        api.Logger.Notification("Hello from template mod: " + api.Side);
     }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        api.Logger.Notification("Hello from template mod server side: " + Lang.Get("_modid_:hello"));
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        api.Logger.Notification("Hello from template mod client side: " + Lang.Get("_modid_:hello"));
+    }
+#endif
 }
