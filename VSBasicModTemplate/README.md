003aa934949bc5579fff32b9c099b7bfbeca6fd5
Th3Dilli <th3dilli@gmx.at>
2024-05-21 14:51:24 +0200

    add basic template

---
+++ VSBasicModTemplate/README.md
@@ -0,0 +1,4 @@
+# Vintage Story Mod Templates
+
+See our mod [examples](https://github.com/anegostudios/vsmodexamples) and our [Wiki](https://wiki.vintagestory.at/index.php/Modding:Setting_up_your_Development_Environment) for more information.
+
---
+++ VSBasicModTemplate/VintageStoryBasicMod/_ProjectName_/_ProjectName_ModSystem.cs
@@ -0,0 +1,28 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace _ProjectName_;
+
+public class _ProjectName_ModSystem : ModSystem
+{
+
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+    public override void Start(ICoreAPI api)
+    {
+        api.Logger.Notification("Hello from template mod: " + api.Side);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        api.Logger.Notification("Hello from template mod server side: " + Lang.Get("_modid_:hello"));
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        api.Logger.Notification("Hello from template mod client side: " + Lang.Get("_modid_:hello"));
+    }
+
+}
